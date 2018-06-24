f0a372dc151f76cfb9371429d446caf4a1b88528
purevee <purevee@purevees-imac.lan>
2018-06-25 06:06:28 +0800

    interface change

--- ErxesSDK/Classes/Global/Extensions.swift
+++ ErxesSDK/Classes/Global/Extensions.swift
@@ -191,7 +191,7 @@ public extension UIFont {
 
 extension String {
     func isValidEmail() -> Bool {
-        let regex = try! NSRegularExpression(pattern: "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", options: .caseInsensitive)
+        let regex = try! NSRegularExpression(pattern: "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}", options: .caseInsensitive)
         return regex.firstMatch(in: self, options: [], range: NSRange(location: 0, length: count)) != nil
     }
     
